03168c9a26376c2dcf97f6e822a51bd4753e10b3
nishap@gemtenderconsultant.com <tenderkhabar@ubuntu-hestia.localhost>
2026-04-21 15:20:32 +0530

    adding api code

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
-use App\Http\Controllers\DataController;
+use App\Http\Controllers\Api\DataController;
 use App\Http\Controllers\AllTablesController;
 
 /*
