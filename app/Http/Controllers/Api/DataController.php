75c1bc798bfd2206e58717efae59139b70619d20
nishap@gemtenderconsultant.com <tenderkhabar@ubuntu-hestia.localhost>
2026-04-21 15:18:49 +0530

    adding api code

---
+++ app/Http/Controllers/Api/DataController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DataController extends Controller
+{ 
+    public function category()
+    {
+        $data = DB::table('keyword')->get();
+
+        return response()->json([
+            'status' => true,
+            'data' => $data
+        ]);
+    }
+    public function authorities()
+    {
+        $data = DB::table('agency')->get();
+
+        return response()->json([
+            'status' => true,
+            'data' => $data
+        ]);
+    }
+    public function state()
+    {
+        $data = DB::table('state')->get();
+
+        return response()->json([
+            'status' => true,
+            'data' => $data
+        ]);
+    }
+// tender
+    public function live_tenders()
+    {
+        $data = DB::table('live_tenders')->get();
+
+        return response()->json([
+            'status' => true,
+            'data' => $data
+        ]);
+    }
+
+    public function livetendercategory()
+    {
+        $data = DB::table('livetendercategory')->get();
+
+        return response()->json([
+            'status' => true,
+            'data' => $data
+        ]);
+    }
+
+    public function tenderinfo_2017()
+    {
+        $data = DB::table('tenderinfo_2017')->get();
+
+        return response()->json([
+            'status' => true,
+            'data' => $data
+        ]);
+    }
+
+    public function tenderinfo_items()
+    {
+        $data = DB::table('tenderinfo_items')->get();
+
+        return response()->json([
+            'status' => true,
+            'data' => $data
+        ]);
+    }
+// tender result
+    public function tender_result_info()
+    {
+        $data = DB::table('tender_result_info')->get();
+
+        return response()->json([
+            'status' => true,
+            'data' => $data
+        ]);
+    }
+    // Customer Plan API
+    public function tender_result_category()
+    {
+        $data = DB::table('tender_result_category')->get();
+
+        return response()->json([
+            'status' => true,
+            'data' => $data
+        ]);
+    }
+
+    // Payments API
+    public function tender_doc()
+    {
+        $data = DB::table('tender_doc')->get();
+
+        return response()->json([
+            'status' => true,
+            'data' => $data
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\DataController;
 use App\Http\Controllers\AllTablesController;
 
 /*
@@ -26,3 +27,14 @@ Route::get('/all-tables', [AllTablesController::class, 'fulldataccess']);
 Route::get('/state-tender-summary', [AllTablesController::class, 'stateTenderSummary']);
 Route::post('/gettendersdata', [AllTablesController::class, 'actionGettendersdata']);
 Route::post('/gettendersdatas', [AllTablesController::class, 'tendersearch']);
+
+Route::get('/category', [DataController::class, 'category']);
+Route::get('/authorities', [DataController::class, 'authorities']);
+Route::get('/state', [DataController::class, 'state']);
+Route::get('/live_tenders', [DataController::class, 'live_tenders']);
+Route::get('/livetendercategory', [DataController::class, 'livetendercategory']);
+Route::get('/tenderinfo_2017', [DataController::class, 'tenderinfo_2017']);
+Route::get('/tenderinfo_items', [DataController::class, 'tenderinfo_items']);
+Route::get('/tender_result_info', [DataController::class, 'tender_result_info']);
+Route::get('/tender_result_category', [DataController::class, 'tender_result_category']);
+Route::get('/tender_doc', [DataController::class, 'tender_doc']);
